8f1629c403a61a663846a302637c302560ea6916
estkon <61468563+estkon@users.noreply.github.com>
2021-07-07 14:15:21 +0200

    Début : middleware formulaire sauce

--- backend/middleware/checkFormSauce.js
+++ backend/middleware/checkFormSauce.js
@@ -1,18 +1,24 @@
-// middleware pour la vérification du formulaire de création de sauce
+// middleware pour la validité du formulaire sauce
+
 
 module.exports = (req,res,next) => {
-    const regexName = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    const regexManufacturer = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/;
-    const regexMainPepper =
+    //Regex
 
-    if(regexEmail.test(req.body.email)) {
-        if(regexPassword.test(req.body.password)) {
+const regexName = /^[0-9a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ.!@&-_\s]{3,20}$/; // a utilisé pour le nom de la sauce et le créateur
+// const regexDescription = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/;
+// const regexMainPepper = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+//le nom de la sauce
+        if(regexName.test(req.body.name)) {
             next();
         } else {
-            res.status(400).json({ message: "Le mot de passe doit contenir au moins 10 caractères, "
-            +"posséder au moins un chiffre, une lettre majuscule et minuscule et l'un des caractères spéciaux suivants: @$!%*?&."});
+            res.status(400).json({ message: "Le nom doit contenir au moins 3 caractères"});
         }
-    } else {
-        res.status(400).json({ message: "Veuillez saisir un email valide !"});
-    }
+
+//le nom du créateur de la sauce
+if(regexName.test(req.body.manufacturer)) {
+    next();
+} else {
+    res.status(400).json({ message: "Le créateur doit contenir au moins 3 caractères"});
 }
+    }
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,13 +5,14 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 const checkFormSauce = require('../middleware/checkFormSauce');
 
+
 const sauceCtrl = require('../controllers/sauce');
 
 
 
 
-router.post('/',auth,  multer, checkFormSauce, sauceCtrl.createSauce );
-router.put('/:id',auth, multer, checkFormSauce, sauceCtrl.modifySauce );
+router.post('/',auth,  multer, checkFormSauce,  sauceCtrl.createSauce );
+router.put('/:id',auth, multer,checkFormSauce, sauceCtrl.modifySauce );
 router.delete('/:id',auth, sauceCtrl.deleteSauce);
 router.get('/:id',auth, sauceCtrl.getOneSauce );
 router.get('/',auth, sauceCtrl.getAllSauces) ;
