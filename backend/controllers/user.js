761072641312d074c8b10cdd844b2b8dc2667764
estkon <61468563+estkon@users.noreply.github.com>
2021-07-09 15:54:59 +0200

    Cryptage de l'email

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,15 +1,21 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken'); 
+const CryptoJS = require('crypto-js');
 
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
-  const regex = 
+
+  //Cryptage de l'email
+  var key = CryptoJS.enc.Hex.parse("000102030405060708090a0b0c0d0e0f");
+  var iv = CryptoJS.enc.Hex.parse("101112131415161718191a1b1c1d1e1f");
+  var encrypted = CryptoJS.AES.encrypt(req.body.email, key, { iv: iv }).toString();
+
   //chiffrage du mot de passe
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
       const user = new User({
-        email: req.body.email,
+        email: encrypted,
         password: hash
       });
       //ajout de l'utilisateur à la base de données
@@ -23,7 +29,11 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
+  var key = CryptoJS.enc.Hex.parse("000102030405060708090a0b0c0d0e0f");
+  var iv = CryptoJS.enc.Hex.parse("101112131415161718191a1b1c1d1e1f");
+  var encrypted = CryptoJS.AES.encrypt(req.body.email, key, { iv: iv }).toString();
+
+  User.findOne({ email: encrypted })
     .then(user => {
       if (!user) {
         return res.status(401).json({ error: 'Utilisateur non trouvé !' });
